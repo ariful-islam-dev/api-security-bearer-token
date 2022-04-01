fbb0b38de722cd6ba47a7a99ec5a5f8b12c0c12f
ariful4082 <ariful4082@gmail.com>
2022-04-01 19:18:28 +0600

    registation process successful

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # API SECURITY (Bearar Token)
 
 **_Backend API_**
+
+### Working Process
+
+- [x] Register User
--- src/authRoutes.js
+++ src/authRoutes.js
@@ -1,10 +1,34 @@
-const router = require('express').Router();
+const User = require("./User");
+const bcrypt = require("bcryptjs");
 
-router.post('/register', (req, res)=>{
+const router = require("express").Router();
 
-})
-router.post('/login', (req, res)=>{
+router.post("/register", async (req, res, next) => {
+  const { name, email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
 
-})
+    if (user) {
+      return res.status(400).json({
+        error: "Email Already Exist",
+      });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-module.exports = router
+    user = new User({
+      name,
+      email,
+      password: hash,
+    });
+
+    await user.save();
+
+    res.status(201).json({ message: "User Create Successful", user });
+  } catch (err) {
+    next(err);
+  }
+});
+router.post("/login", (req, res) => {});
+
+module.exports = router;
